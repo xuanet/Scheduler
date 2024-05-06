681553a78fcd52b0f8404fc6eb24a6ed464843ba
xuanet <89868629+xuanet@users.noreply.github.com>
2024-05-06 03:42:31 -0400

    Add files via upload

---
+++ models/Girl.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const girlSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }, 
+    height: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    specialty: {
+        type: String,
+        required: true
+    },
+    race: {
+        type: String,
+        required: true
+    },
+    comments: {
+        type: String,
+        required: true
+    }
+});
+
+const Girl = mongoose.model('Girl', girlSchema);
+module.exports = Girl;
